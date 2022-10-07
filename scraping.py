5b98eb41fa1b661d09cfcf7d094b90121ed0fe7e
lovelyBestDev <lovelyblueshark@gmail.com>
2022-10-08 06:18:28 +0900

    update_script

--- scraping.py
+++ scraping.py
@@ -1,15 +1,8 @@
-
-# import pandas lib as pd
-# from importlib.metadata import requires
-# import pandas as pd
 from pickle import GLOBAL
 from tkinter import font
 from turtle import left, width
 import openpyxl
-# import requests
-# from bs4 import BeautifulSoup
 from selenium import webdriver
-# from selenium.webdriver.common.by import By
 from tkinter import *
 from tkinter.ttk import *
 
@@ -67,7 +60,6 @@ def fileScraping():
     dataframe.save("output.xlsx")
     
     if len(searchResult) > 0:
-        # label_page['text'] = 1
         display()
 
 
@@ -133,19 +125,9 @@ def searchTypeChange(event):
         
         label_searchProperty_name.place_forget()
         label_searchProperty_company.place_forget()
-        # input_name.place(x=50, y=150)
-        # input_name.config(width=15)
-        # input_company.place(x=50, y=180)
-        # input_company.config(width=15)
-        # text_result.place(x=150, y=150)
-        # text_result.config(width=40, height=11)
         input_name.place_forget()
         input_company.place_forget()
         text_result.place_forget()
-        
-        # btn_next.place(x=280, y=120)
-        # label_page.place(x=255, y=122)
-        # btn_before.place(x=220, y=120)
         
         btn_run.place(x=500, y=80)
         btn_run.config(width=15)
@@ -156,26 +138,17 @@ def searchTypeChange(event):
         label_filePath['text'] = ""
         display_table.delete(*display_table.get_children())
         
-        # label_page['text'] = '0'
-        
         root.geometry('500x500')
         btn_open.place_forget()
         label_filePath.place_forget()
         frame.place_forget()
         
-        # btn_next.place_forget()
-        # label_page.place_forget()
-        # btn_before.place_forget()
-        
         label_searchProperty_name.place(x=30, y=82)
         label_searchProperty_company.place(x=20, y=120)
         
         input_name.place(x=100, y=82)
-        # input_name.config(width=60)
         input_company.place(x=100, y=120)
-        # input_company.config(width=60)
         text_result.place(x=100, y=150)
-        # text_result.config(width=45, height=15)
         
         btn_run.place(x=50, y=430)    
         btn_run.config(width=68)  
@@ -270,66 +243,6 @@ label_searchProperty_company = ttk.Label(
 label_searchProperty_company.pack(expand=True)
 label_searchProperty_company.pack_forget()
 
-# def display():
-#     global searchResult
-    
-#     input_name.delete(0, END)
-#     input_company.delete(0, END)
-#     text_result.delete("1.0", 'end')
-    
-#     input_name.insert(END, searchResult[int(label_page['text']) - 1][0])
-#     input_company.insert(END, searchResult[int(label_page['text']) - 1][1])
-#     text_result.insert(END, searchResult[int(label_page['text']) - 1][2] + "\n\n" + searchResult[int(label_page['text']) - 1][3])
-
-# def next():
-#     global searchResult
-#     if label_page['text'] == len(searchResult) or searchResult == [] or label_page['text'] == 0:
-#         return
-    
-#     label_page['text'] = int(label_page['text']) + 1
-    
-#     display()
-
-# def before():
-#     global searchResult
-#     if label_page['text'] == 1 or label_page['text'] == 0:
-#         return
-    
-#     label_page['text'] = int(label_page['text']) - 1
-    
-#     display()
-
-
-# btn_next = ttk.Button(
-#     root,
-#     text='>',
-#     width=2,
-#     command=next
-# )
-# btn_next.pack()
-# btn_next.place(x=280, y=120)
-
-
-# label_page = ttk.Label(
-#     root,
-#     text='0',
-#     width=1,
-#     font=14,
-# )
-# label_page.pack(expand=True)
-# label_page.place(x=255, y=122)
-
-
-# btn_before = ttk.Button(
-#     root,
-#     text='<',
-#     width=2,
-#     command=before
-# )
-# btn_before.pack()
-# btn_before.place(x=220, y=120)
-
-
 
 # run button
 btn_run = ttk.Button(
@@ -352,9 +265,7 @@ def display():
     for i in range(0, len(searchResult)):
         display_table.insert(parent='', index='end', iid=i, text='', 
             values=((i+1), searchResult[i][0], searchResult[i][1], searchResult[i][2], searchResult[i][3]))
-    #     return
-    # return
-
+        
 
 frame = Frame(root)
 frame.pack()
@@ -378,22 +289,7 @@ display_table.heading("item_company",text="Company",anchor=CENTER)
 display_table.heading("item_position",text="Position",anchor=CENTER)
 display_table.heading("item_url",text="Url",anchor=CENTER)
 
-# display_table.insert(parent='',index='end',iid=0,text='',
-# values=('1','Ninja','101','Oklahoma', 'Moore'))
-# display_table.insert(parent='',index='end',iid=1,text='',
-# values=('2','Ranger','102','Wisconsin', 'Green Bay'))
-# display_table.insert(parent='',index='end',iid=2,text='',
-# values=('3','Deamon','103', 'California', 'Placentia'))
-# display_table.insert(parent='',index='end',iid=3,text='',
-# values=('4','Dragon','104','New York' , 'White Plains'))
-# display_table.insert(parent='',index='end',iid=4,text='',
-# values=('5','CrissCross','105','California', 'San Diego'))
-# display_table.insert(parent='',index='end',iid=5,text='',
-# values=('6','ZaqueriBlack','106','Wisconsin' , 'TONY'))
-
 display_table.pack()
-
-
 
 
 # run the application
